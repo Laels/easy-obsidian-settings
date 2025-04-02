77d28cb9a63bb5ca8664868ea7f20e31305c2494
Lael <lael.xuls@gmail.com>
2025-04-02 15:41:23 +0800

    4.2

--- .obsidian/plugins/calendar/popper.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "@popperjs/svelte";
--- .obsidian/plugins/calendar/src/ui/sources/index.ts
+++
@@ -1,6 +0,0 @@
-export { backlinksSource } from "./backlinks";
-export { emojiTagsSource } from "./emojiTags";
-export { linksSource } from "./links";
-export { tasksSource } from "./tasks";
-export { wordCountSource } from "./wordCount";
-export { zettelsSource } from "./zettels";
